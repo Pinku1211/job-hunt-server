5fc18b5579caf5ca33905f1e0cf0c69fa3f88284
Pinku1211 <pinkukumarsarker@gmail.com>
2023-11-08 12:55:29 +0600

    implement jwt and generate token

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const cors = require('cors');
+const jwt = require('jsonwebtoken');
+const cookieParser = require('cookie-parser')
 
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
@@ -8,10 +10,15 @@ const port = process.env.PORT || 5000;
 
 // middleware
 
-app.use(cors());
+app.use(cors({
+  origin: [
+    'http://localhost:5173'
+  ],
+  credentials: true
+}));
 app.use(express.json());
+app.use(cookieParser());
 
-console.log(process.env.DB_PASS)
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.r8pib.mongodb.net/?retryWrites=true&w=majority`;
 
@@ -24,6 +31,12 @@ const client = new MongoClient(uri, {
   }
 });
 
+// middlewares 
+const logger = (req, res, next) => {
+  console.log('log: info',req.method, req.url);
+  next();
+};
+
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
@@ -32,6 +45,27 @@ async function run() {
     const jobCollection = client.db('jobHunt').collection('jobs');
     const applicantCollection = client.db('jobHunt').collection('applicants');
 
+    // auth api
+    app.post('/jwt',async(req, res)=>{
+      const user = req.body;
+      console.log('user for token', user);
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {expiresIn: '1h'})
+      res.cookie('token', token, {
+        httpOnly: true,
+        secure: true,
+        sameSite: 'none'
+      })
+      .send({token})
+    })
+
+    app.post('/logout', async(req, res) => {
+      const user = req.body;
+      console.log('logging out', user)
+      res.clearCookie('token', {maxAge: 0}).send({success: true})
+
+    })
+
+    // services api
     app.get('/jobs', async (req, res) => {
       const cursor = jobCollection.find();
       const result = await cursor.toArray();
@@ -44,7 +78,7 @@ async function run() {
       res.send(result)
     })
 
-    app.get('/jobs/:id', async (req, res) => {
+    app.get('/jobs/:id', logger, async (req, res) => {
       const id = req.params.id;
       const query = { _id: new ObjectId(id) };
       const result = await jobCollection.findOne(query);
@@ -62,34 +96,50 @@ async function run() {
       res.send(result)
     })
 
-    app.delete('/jobs/:id', async(req, res)=> {
+    app.delete('/jobs/:id', async (req, res) => {
       const id = req.params.id;
-      const query = {_id: new ObjectId(id)};
+      const query = { _id: new ObjectId(id) };
       const result = await jobCollection.deleteOne(query)
       res.send(result)
-  })
+    })
 
-  app.put('/jobs/:id', async(req, res)=>{
-    const id = req.params.id;
-    const filter = {_id: new ObjectId(id)};
-    const options = {upsert: true};
-    const updatedJob = req.body;
-    const job = {
+    app.put('/jobs/:id', async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+      const options = { upsert: true };
+      const updatedJob = req.body;
+      const job = {
         $set: {
-            job_banner: updatedJob.job_banner,
-            name_posted: updatedJob.name_posted,
-            job_title: updatedJob.job_title,
-            job_category: updatedJob.job_category,
-            salary_range: updatedJob.salary_range,
-            description: updatedJob.description,
-            job_posting_date: updatedJob.job_posting_date,
-            application_deadline: updatedJob.application_deadline,
-            applicants_number: updatedJob.applicants_number,
+          job_banner: updatedJob.job_banner,
+          name_posted: updatedJob.name_posted,
+          job_title: updatedJob.job_title,
+          job_category: updatedJob.job_category,
+          salary_range: updatedJob.salary_range,
+          description: updatedJob.description,
+          job_posting_date: updatedJob.job_posting_date,
+          application_deadline: updatedJob.application_deadline,
+          applicants_number: updatedJob.applicants_number,
         }
-    }
-    const result = await jobCollection.updateOne(filter, job, options);
-    res.send(result)
-})
+      }
+      const result = await jobCollection.updateOne(filter, job, options);
+      res.send(result)
+    })
+
+    // app.put('/jobs/:id', async (req, res) => {
+    //   const id = req.params.id;
+    //   const query = { _id: new ObjectId(id) };
+    //   const job = {
+    //     $inc: {
+    //       applicants_number: 1
+    //     }
+    //   }
+    //   const result = await jobCollection.updateOne(query, job);
+    //   res.send(result)
+    // })
+
+    
+
+
 
 
     // Send a ping to confirm a successful connection
